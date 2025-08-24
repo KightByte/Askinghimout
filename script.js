0201cf534fd415e8bb08c89192acaea97161e83d
KightByte <caseylaymon2@gmail.com>
2025-08-24 17:24:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// Change the image when clicked
+document.getElementById('noteImage').addEventListener('click', function() {
+    const image = document.getElementById('noteImage');
+    if (image.src.includes('note1.png')) {
+        image.src = 'note2.png';  // Change to second image
+    } else {
+        image.src = 'note1.png';  // Revert back to first image
+    }
+});
 
